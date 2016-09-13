519d0d44804ea71e4c8bba14987ef26f8c835596
SELWYN GRACE-FOSTER <selwyngracefoster@adelphi.internal>
2016-09-13 12:46:39 -0400

    level 5 done

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -6,6 +6,60 @@
  * @version (a version number or a date)
  */
 public class PersonCreator
-{
-    // Your class begins here!
+{   // Name of the person
+    private String name;
+    // Age of the person
+    private int age; 
+   // Sex of the person
+    private String sex;
+     // Height of the person
+    private int height;
+    private int weight;
+    private String SSN;
+    private String address;
+    private String phoneNum;
+    private int first, second, last;
+    private int Areacode, prefix, newlast;
+    private String DOB;
+    
+    public void PersonCreator( String firstname, String lastname){
+        name=firstname+" "+lastname;
+    }
+    
+    public void setName(String newName){
+        name=newName;
+    }
+    public void setAge( int newAge) {
+        age=newAge;
+    }
+    public void setSex( String newSex) { 
+        sex=newSex;
+    }
+    public void setHeight( int newHeight) {
+        height=newHeight;
+    }
+    public void setWeight( int newWeight) {
+        weight=newWeight;
+    }
+    public void setSSN(String newSSN) {
+        SSN=newSSN;
+    }
+    public void setAddress(String newaddress) {
+        address=newaddress;
+    }
+    public void setPhoneNum(String newphoneNum) {
+        phoneNum=newphoneNum;
+        if (first<100 && first>999){
+            Areacode=first;
+        }
+             if(second<100 && second>999) {
+                prefix=second;}
+                if (last<1000&& last>9999) {last=newlast;}
+                    
+    }
+    public void setDOB( String newDOB){
+        DOB=newDOB;
+    }
 }
+
+    
